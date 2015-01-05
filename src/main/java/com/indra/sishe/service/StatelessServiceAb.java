045aba173fbc1e65035bfc906eaba6957e7efee7
fabmarc <fabio.mfp@gmail.com>
2015-01-05 17:15:24 -0300

    Created service inhetance

---
+++ src/main/java/com/indra/sishe/service/StatelessServiceAb.java
@@ -0,0 +1,10 @@
+package com.indra.sishe.service;
+
+import javax.interceptor.Interceptors;
+
+import org.springframework.ejb.interceptor.SpringBeanAutowiringInterceptor;
+
+@Interceptors(SpringBeanAutowiringInterceptor.class)
+public class StatelessServiceAb {
+
+}
