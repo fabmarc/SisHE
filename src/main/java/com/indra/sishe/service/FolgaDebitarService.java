d89ca2e8102881d2cecfc29c73a0f33700e32f2b
isouzaa <isouzaa@indracompany.com>
2015-06-17 17:00:07 -0300

    Criação dos Services de: FolgaDebitar e HistoricoFolga

---
+++ src/main/java/com/indra/sishe/service/FolgaDebitarService.java
@@ -0,0 +1,8 @@
+package com.indra.sishe.service;
+
+import com.indra.infra.service.BaseService;
+import com.indra.sishe.entity.FolgaDebitar;
+
+public interface FolgaDebitarService extends BaseService<FolgaDebitar> {
+	
+}
